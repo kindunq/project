b981311c388459b3370ad3e9eed0432642a376c0
kindunq <pinkeman@naver.com>
2021-04-06 14:55:49 +0900

    tasks menu add

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('tasks', \App\Http\Controllers\TaskController::class);
